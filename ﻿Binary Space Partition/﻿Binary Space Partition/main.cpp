b4678cb328923325ba909d3de219f94857fc51ec
SimJoonTTMD <wesley0940@gmail.com>
2021-01-07 19:58:55 +0900

    Binary Space Partition_Release 2

--- ﻿Binary Space Partition/﻿Binary Space Partition/main.cpp
+++ ﻿Binary Space Partition/﻿Binary Space Partition/main.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <algorithm>
 #include <string.h>
+#include <queue>
 using namespace std;
 struct Node {
 	int x, y;
@@ -16,22 +17,29 @@ int arr[110][110];
 int x, y, cnt;
 char file_buff[101 * 101];
 FILE* fp;
+int dx[10] = { 0,0,0,1,1,1,-1,-1,-1 };
+int dy[10] = { 1,0,-1,1,0,-1,1,0,-1 };
+vector< pair<Node,Node> > mid_check;
 
 void print_map();
 void push_map();
 void scan_map();
 void bsp(Node st, Node en);
+void connect();
+void check_connected();
+
 
 int main() {
 	srand(time(NULL));
 	Node a, b;
-	a.x = 0;
-	a.y = 0;
-	b.x = 100;
-	b.y = 100;
+	a = { 0,0 };
+	b = { 100,100 };
 	bsp(a, b);
 	print_map();
 	push_map();
+	printf("\n");
+	connect();
+	print_map();
 	return 0;
 }
 
@@ -41,6 +49,8 @@ void print_map() {
 			if (i == 0 || i == 99 || j == 0 || j == 99) arr[i][j] = 1;
 			if (arr[i][j] == 0) printf("□");
 			else if (arr[i][j] == 1) printf("■");
+			else if (arr[i][j] == 2) printf("◆");
+			else printf("%2d", arr[i][j]);
 		}
 		printf("\n");
 	}
@@ -77,6 +87,7 @@ void scan_map() {
 }
 
 void bsp(Node st,Node en) {
+	int flag;
 //	printf("%d %d %d %d", st.x, st.y, en.x, en.y);
 	for (int i = st.x; i < en.x; i++) {
 		for (int j = st.y; j<en.y; j++) {
@@ -87,11 +98,18 @@ void bsp(Node st,Node en) {
 	mid.x = (st.x + en.x) / 2;
 	mid.y = (st.y + en.y) / 2;
 	if (abs(en.x - st.x) * abs(en.y - st.y) < 400) {
+		//for (int i = st.x + 1; i < en.x - 1; i++) {
+		//	for (int j = st.y + 1; j < en.y - 1; j++) {
+		//	//	arr[i][j] = 2;
+		//	}
+		//}
+		mid_check.push_back({ st,en });
 		return;
 	}
 	else {
-		if (rand() % 2 == 0) { //세로로 선 긋기
-			if (en.x - st.x > 8) {
+		flag = rand() % 2;
+		if (flag == 0) { //세로로 선 긋기
+			if (en.x - st.x > 6) {
 				mid.x -= 3;
 				mid.x += rand() % 7;
 			}
@@ -100,7 +118,7 @@ void bsp(Node st,Node en) {
 			}
 		}
 		else {
-			if (en.y - st.y > 8) {
+			if (en.y - st.y > 6) {
 				mid.y -= 3;
 				mid.y += rand() % 7;
 			}
@@ -108,6 +126,8 @@ void bsp(Node st,Node en) {
 				arr[i][mid.y] = 1;
 			}
 		}
+		
+
 //		printf(" %d %d\n", mid.x, mid.y);
 		Node nd1, nd2;
 		nd1 = st;
@@ -122,8 +142,75 @@ void bsp(Node st,Node en) {
 		nd1 = { mid.x,mid.y};
 		nd2 = en;
 		bsp(nd1, nd2); //우측 하단
-
-
+		/*if (flag == 0) {
+			nd1.x = (st.x + mid.x+1) / 2;
+			for (int i = 0; i < 9; i++) {
+				arr[nd1.x + dx[i]][mid.y + dy[i]] = 0;
+			}
+		}
+		else{
+			nd1.y = (st.y + mid.y+1) / 2;
+			for (int i = 0; i < 9; i++) {
+				arr[nd1.x + dx[i]][nd1.y + dy[i]] = 0;
+			}
+		}*/		
 	}
 	return;
 }
+
+void connect() {
+	Node st,en,mid;
+	int vl = mid_check.size();
+	int dis;
+	for (int i = 0; i < vl; i++) {
+		dis = rand() % 4;
+		st = mid_check[i].first;
+		en = mid_check[i].second;
+		en.x--;
+		en.y--;
+		mid = { (st.x + en.x) / 2,(st.y + en.y) / 2 };
+		
+		if (dis != 0) {
+			arr[st.x][mid.y] = 0;
+		}
+		if (dis != 1) {
+			arr[mid.x][en.y] = 0;
+		}
+		if (dis != 2) {
+			arr[en.x][mid.y] = 0;
+		}
+		if (dis != 3) {
+			arr[mid.x][st.y] = 0;
+		}
+		/*
+		arr[st.x][st.y] = 2;
+		arr[st.x][en.y] = 2;
+		arr[en.x][st.y] = 2;
+		arr[en.x][en.y] = 2;
+		arr[st.x][mid.y] = (mid_check[i].first.x+ mid_check[i].first.y)%100;
+		arr[mid.x][en.y] = (mid_check[i].first.x+ mid_check[i].first.y)%100;
+		arr[en.x][mid.y] = (mid_check[i].first.x+ mid_check[i].first.y)%100;
+		arr[mid.x][st.y] = (mid_check[i].first.x+ mid_check[i].first.y)%100;
+		arr[mid.x][mid.y] = dis + 10;*/
+	}
+}
+
+void check_connected() {
+	queue<Node> q;
+	Node now,next;
+	q.push({ 1,1 });
+	int dx2[5] = { 0,0,1,-1 };
+	int dy2[5] = { 1,-1,0,0 };
+	while (!q.empty()) {
+		now = q.front();
+		q.pop();
+		for (int i = 0; i < 4; i++) {
+			next = { now.x + dx2[i],now.y + dy2[i] };
+			if (arr[next.x][next.y] == 0) {
+				arr[next.x][next.y] = 2;
+				q.push(next);
+			}
+		}
+	}
+}
+
